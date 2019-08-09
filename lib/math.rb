a03cc6c306b500d6b6753750cce1e531d14d6282
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 14:21:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,11 +3,11 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  return num2 - num1
+  return num1 - num2
 end
 
 def division(num1, num2)
-  return Float(num2)/Float(num1)
+  return Float(num1)/Float(num2)
 end
 
 def multiplication(num1, num2)
