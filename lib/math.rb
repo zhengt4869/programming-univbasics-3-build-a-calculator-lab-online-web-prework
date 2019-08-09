3f04c0f04a5541fd1a0ecf5ffc09737f3286f71c
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 14:20:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,18 +10,14 @@ def division(num1, num2)
   return Float(num2)/Float(num1)
 end
 
-def modulo(num1, num2)
-  return num2 % num1
-end
-
 def multiplication(num1, num2)
   return num1 * num2
 end
 
 def modulo(num1, num2)
-
+  return num2 % num1
 end
 
 def square_root(num)
-
+  return num ** .5
 end
